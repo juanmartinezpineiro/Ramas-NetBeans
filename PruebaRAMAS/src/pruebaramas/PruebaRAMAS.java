65ebae2a7a557c84dbfec1eeb556261f960e3e1e
jmartinezpineiro <jmartinezpineiro@nautilus16.danielcastelao.org>
2018-11-08 09:23:17 +0100

    Primer commit

---
+++ PruebaRAMAS/src/pruebaramas/PruebaRAMAS.java
@@ -0,0 +1,27 @@
+package pruebaramas;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jmartinezpineiro
+ */
+public class PruebaRAMAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int num1,num2,suma;
+        
+        num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca num1: "));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca segundo numero: "));
+        
+        suma = num1 + num2;
+        
+        //Commit 1
+
+    }
+
+}
